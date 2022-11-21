5be4ad4f0487ac4a4d5cd52fe52344840f6d828d
Mayo1590 <2035059@cgmatane.qc.ca>
2022-11-21 16:42:57 -0500

    poc #17

--- platforms/android/app/src/main/assets/www/api/PositionSQL.php
+++ platforms/android/app/src/main/assets/www/api/PositionSQL.php
@@ -1,5 +1,5 @@
 <?php
 interface PositonSQL {
     public const SQL_LISTER_POSITION = 'SELECT * FROM position WHERE id_endroit = :id_endroit';
-    public const SQL_AJOUTER_POSITION = 'INSERT INTO position(longitude, latitude, id_endroit) VALUES(:longitude, :latitude, :id_endroit)';
+    public const SQL_AJOUTER_POSITION = 'INSERT INTO position(longitude, latitude) VALUES(:longitude, :latitude)';
 }
